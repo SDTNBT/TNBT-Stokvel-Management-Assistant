8ff2247724d2da371982ad14d87bb8eb9ec9c523
GomolemoRampa <wisemanrampa@gmail.com>
2026-05-12 16:27:01 +0200

    Conflict resolved

--- backend/routes/bankingRoutes.js
+++ backend/routes/bankingRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< Updated upstream
-const verifyFirebaseToken = require('../middleware/authMiddleware');
-=======
 const  verifyFirebaseToken  = require('../middleware/authMiddleware');
->>>>>>> Stashed changes
 const { getSABanks, saveBankingDetails , getBankingDetails} = require('../controllers/bankingController');
 
 
